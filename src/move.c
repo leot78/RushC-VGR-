9d7993d14e9a2f060ba1b2372161cb8cdc13811d
Valentin Ouvrard <valentin.ouvrard@epita.fr>
2017-12-02 15:51:21 +0100

    [MOVE] define SIZE

--- src/move.c
+++ src/move.c
@@ -3,6 +3,8 @@
 #include "object.h"
 #include "player.h"
 
+#define SIZE 32
+
 void player_move_up(struct player *p, struct map *map)
 {
   if (p->y != 0) 
@@ -11,7 +13,7 @@ void player_move_up(struct player *p, struct map *map)
     if (obj->type == NONE)
     {
       --p->y;
-      p->rect = init_rect(p->x * 16, p->y * 16, 16, 16);
+      p->rect = init_rect(p->x * SIZE, p->y * SIZE, SIZE, SIZE);
     }
     else if (obj->type == PC && obj->state == 0)
       obj->color = pick_color(BLUE);
@@ -23,7 +25,7 @@ void player_move_down(struct player *p, struct map *map)
   if (p->y + 1 < map->height && map->objs[p->x][p->y + 1]->type == NONE)
   {
     ++p->y;
-    p->rect = init_rect(p->x * 16, p->y * 16, 16, 16);
+    p->rect = init_rect(p->x * SIZE, p->y * SIZE, SIZE, SIZE);
   }
 }
 
@@ -32,7 +34,7 @@ void player_move_right(struct player *p, struct map *map)
   if (p->x + 1 < map->width && map->objs[p->x + 1][p->y]->type == NONE)
   {
     ++p->x;
-    p->rect = init_rect(p->x * 16, p->y * 16, 16, 16);
+    p->rect = init_rect(p->x * SIZE, p->y * SIZE, SIZE, SIZE);
   }
 }
 
@@ -41,6 +43,6 @@ void player_move_left(struct player *p, struct map *map)
   if (p->x != 0 && map->objs[p->x - 1][p->y]->type == NONE)
   {
     --p->x;
-    p->rect = init_rect(p->x * 16, p->y * 16, 16, 16);
+    p->rect = init_rect(p->x * SIZE, p->y * SIZE, SIZE, SIZE);
   }
 }

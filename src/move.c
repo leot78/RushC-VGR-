eda9abe70b0251218d8e122732cc088dd74f4e5b
Antoine Léauté <antoine.leaute@epita.fr>
2017-12-02 18:08:36 +0100

    [MOVE] fix bug with START

--- src/move.c
+++ src/move.c
@@ -7,7 +7,8 @@
 
 int player_move_up(struct player *p, struct map *map)
 {
-  if (p->y != 0 && map->objs[p->x][p->y - 1]->type == NONE)
+  if (p->y != 0 && (map->objs[p->x][p->y - 1]->type == NONE
+      || map->objs[p->x][p->y - 1]->type == START) )
   {
     --p->y;
     p->rect = init_rect(p->x * SIZE, p->y * SIZE, SIZE, SIZE);
@@ -18,7 +19,8 @@ int player_move_up(struct player *p, struct map *map)
 
 int player_move_down(struct player *p, struct map *map)
 {
-  if (p->y + 1 < map->height && map->objs[p->x][p->y + 1]->type == NONE)
+  if (p->y + 1 < map->height && (map->objs[p->x][p->y + 1]->type == NONE
+      || map->objs[p->x][p->y + 1]->type == START) )
   {
     ++p->y;
     p->rect = init_rect(p->x * SIZE, p->y * SIZE, SIZE, SIZE);
@@ -29,7 +31,8 @@ int player_move_down(struct player *p, struct map *map)
 
 int player_move_right(struct player *p, struct map *map)
 {
-  if (p->x + 1 < map->width && map->objs[p->x + 1][p->y]->type == NONE)
+  if (p->x + 1 < map->width && (map->objs[p->x + 1][p->y]->type == NONE
+      || map->objs[p->x + 1][p->y]->type == START) )
   {
     ++p->x;
     p->rect = init_rect(p->x * SIZE, p->y * SIZE, SIZE, SIZE);
@@ -40,7 +43,8 @@ int player_move_right(struct player *p, struct map *map)
 
 int player_move_left(struct player *p, struct map *map)
 {
-  if (p->x != 0 && map->objs[p->x - 1][p->y]->type == NONE)
+  if (p->x != 0 && (map->objs[p->x - 1][p->y]->type == NONE
+      || map->objs[p->x - 1][p->y]->type == START) )
   {
     --p->x;
     p->rect = init_rect(p->x * SIZE, p->y * SIZE, SIZE, SIZE);

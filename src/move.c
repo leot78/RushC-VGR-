74e0077243c6c04dd252774f7c1d9861841f6122
Antoine Léauté <antoine.leaute@epita.fr>
2017-12-02 17:59:05 +0100

    [MOVE] change move functions

--- src/move.c
+++ src/move.c
@@ -5,44 +5,46 @@
 
 #define SIZE 32
 
-void player_move_up(struct player *p, struct map *map)
+int player_move_up(struct player *p, struct map *map)
 {
-  if (p->y != 0) 
+  if (p->y != 0 && map->objs[p->x][p->y - 1]->type == NONE)
   {
-    struct object *obj = map->objs[p->x][p->y - 1];
-    if (obj->type == NONE)
-    {
-      --p->y;
-      p->rect = init_rect(p->x * SIZE, p->y * SIZE, SIZE, SIZE);
-    }
-    else if (obj->type == PC && obj->state == 0)
-      obj->color = pick_color(BLUE);
+    --p->y;
+    p->rect = init_rect(p->x * SIZE, p->y * SIZE, SIZE, SIZE);
+    return 1;
   }
+  return 0;
 }
 
-void player_move_down(struct player *p, struct map *map)
+int player_move_down(struct player *p, struct map *map)
 {
   if (p->y + 1 < map->height && map->objs[p->x][p->y + 1]->type == NONE)
   {
     ++p->y;
     p->rect = init_rect(p->x * SIZE, p->y * SIZE, SIZE, SIZE);
+    return 1;
   }
+  return 0;
 }
 
-void player_move_right(struct player *p, struct map *map)
+int player_move_right(struct player *p, struct map *map)
 {
   if (p->x + 1 < map->width && map->objs[p->x + 1][p->y]->type == NONE)
   {
     ++p->x;
     p->rect = init_rect(p->x * SIZE, p->y * SIZE, SIZE, SIZE);
+    return 1;
   }
+  return 0;
 }
 
-void player_move_left(struct player *p, struct map *map)
+int player_move_left(struct player *p, struct map *map)
 {
   if (p->x != 0 && map->objs[p->x - 1][p->y]->type == NONE)
   {
     --p->x;
     p->rect = init_rect(p->x * SIZE, p->y * SIZE, SIZE, SIZE);
+    return 1;
   }
+  return 0;
 }
--- src/player.h
+++ src/player.h
@@ -17,9 +17,9 @@ struct player
 struct player *player_create(int x, int y, int life);
 void player_delete(struct player *p);
 
-void player_move_up(struct player *p, struct map *map);
-void player_move_down(struct player *p, struct map *map);
-void player_move_right(struct player *p, struct map *map);
-void player_move_left(struct player *p, struct map *map);
+int player_move_up(struct player *p, struct map *map);
+int player_move_down(struct player *p, struct map *map);
+int player_move_right(struct player *p, struct map *map);
+int player_move_left(struct player *p, struct map *map);
 
 #endif /* !PLAYER_H */

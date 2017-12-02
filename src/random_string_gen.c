388ca8f8ce0200e7291fa8ae035185ede6f201cf
Wentzler Quentin <wentzler.quentin@epita.fr>
2017-12-02 02:20:03 +0100

    [STRING] added function to generate a random string

---
+++ src/random_string_gen.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+** Generate a string with random char between int and max
+** (Adviced min = 97, max = 123)
+*/
+char *generate_string(size_t len, int min, int max)
+{
+  static int seeded = 0;
+  if (!seeded)
+  {
+    srand(time(NULL));
+    seeded = 1;
+  }
+  int offset = max - min;
+  char *res = malloc(sizeof(char) * len);
+  for (size_t i = 0; i < len; i++)
+    res[i] = (rand() % offset) + min;
+  return res;
+}

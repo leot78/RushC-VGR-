a318ae08d7b9894a31b8f1b63ee6bc87fd930e79
Valentin Ouvrard <valentin.ouvrard@epita.fr>
2017-12-02 15:50:11 +0100

    [PLAYER] Adding size define

--- src/player.c
+++ src/player.c
@@ -4,6 +4,8 @@
 #include "msdl.h"
 #include "player.h"
 
+#define SIZE 32
+
 struct player *player_create(int x, int y, int life)
 {
   struct player *p = malloc(sizeof(struct player));
@@ -11,7 +13,7 @@ struct player *player_create(int x, int y, int life)
   p->x = x;
   p->y = y;
   p->life = life;
-  p->rect = init_rect(16, 16, x * 16, y * 16);
+  p->rect = init_rect(x * SIZE, y* SIZE, SIZE, SIZE);
   p->color = pick_color(RED);
   return p;
 }

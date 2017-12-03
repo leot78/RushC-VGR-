a47e8ee750107c6106aa0bc56eccc8bb54ecd303
Antoine Léauté <antoine.leaute@epita.fr>
2017-12-03 05:15:07 +0100

    [TIMER] modify timer

--- src/timer.c
+++ src/timer.c
@@ -4,25 +4,15 @@
 
 #include "timer.h"
 
-struct timer g_time;
+time_t g_time;
 
 void init_time(void)
 {
-  g_time.ref = time(NULL);
+  g_time = time(NULL);
 }
 
-char *get_time(void)
+time_t get_time(void)
 {
   time_t cur = time(NULL);
-  if (!g_time.str)
-    g_time.str = malloc(10);
-
-  sprintf(g_time.str, "%ld", cur - g_time.ref);
-  return g_time.str;
-}
-
-void free_timer(void)
-{
-  if (g_time.str)
-    free(g_time.str);
+  return 100 - cur - g_time;
 }
--- src/timer.h
+++ src/timer.h
@@ -3,14 +3,7 @@
 
 #include <time.h>
 
-struct timer
-{
-  time_t ref;
-  char *str;
-};
-
-char *get_time(void);
+time_t get_time(void);
 void init_time(void);
-void free_timer(void);
 
 #endif /* !TIMER_H */
